4e3a75dc68258534953a2ea14c1c9f799ab1568a
KittNika <kittnika@gmail.com>
2013-09-08 20:17:37 -0500

    Part 2 Update

---
+++ src/edu/grinnell/csc207/nikakath/hw2/TwoB.java
@@ -0,0 +1,11 @@
+package edu.grinnell.csc207.nikakath.hw2;
+
+public class TwoB {
+    /**
+     * Compute the average of two integers.  Rounds toward zero if the
+     * average is not a whole number.
+     */
+    public static int average(int left, int right) {
+        return (left + right) / 2;
+    } // average(int,int)
+} // class TwoB
